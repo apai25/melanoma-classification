8a3cc48d21ebfa009600ad9fa6690019d8a0903f
Anirudh Pai <anirudhnpai@gmail.com>
2020-11-07 12:08:14 -0800

    Created predict.py

---
+++ predict.py
@@ -0,0 +1,23 @@
+"""
+Predicts whether a single melanona sample is benign or malignant 
+"""
+from tensorflow.keras.models import load_model
+import numpy as np 
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+
+image_path = 'data/test/non_fire/non_fire.195.png'
+
+def predict(image_path):
+    cnn = load_model('model')
+    image = load_img(image_path, target_size=(128, 128))
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+    prediction = int(cnn.predict(image)[0][0])
+    return prediction
+
+if __name__ == '__main__':
+    prediction = predict(image_path)
+    if prediction == 1:
+        print('Malignant')
+    elif prediction == 0:
+        print('Benign')
