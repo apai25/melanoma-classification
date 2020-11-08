f3c591d06b2e213778949f86f091cfb327f75041
Anirudh Pai <anirudhnpai@gmail.com>
2020-11-08 14:10:15 -0800

    Fixed predict.py labels

--- predict.py
+++ predict.py
@@ -5,7 +5,7 @@ from tensorflow.keras.models import load_model
 import numpy as np 
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
-image_path = 'data/test/malignant/532.jpg'
+image_path = 'data/test/malignant/1.jpg'
 
 def predict(image_path):
     cnn = load_model('model')
@@ -17,7 +17,7 @@ def predict(image_path):
 
 if __name__ == '__main__':
     prediction = predict(image_path)
-    if prediction == 1:
-        print('The tumor is malignant (1.0)')
-    elif prediction == 0:
-        print('The tumor is benign (0.0)')
+    if prediction == 0:
+        print('The tumor is malignant (0.0)')
+    elif prediction == 1:
+        print('The tumor is benign (1.0)')
