d04f40cdd722f2a22f2a1183010800784bf6a341
Anirudh Pai <anirudhnpai@gmail.com>
2020-11-08 21:06:49 -0800

    Added front-end functionality

--- backend/predict.py
+++ backend/predict.py
@@ -1,23 +1,51 @@
 """
 Predicts whether a single melanona sample is benign or malignant 
 """
+
 from tensorflow.keras.models import load_model
 import numpy as np 
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
+import firebase
+import pyrebase 
+from google.cloud import storage
+from google.cloud.storage import client
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import storage
+import glob 
+
+config  = {
+  'apiKey': 'AIzaSyDW2-_l6ALgHftWD6w3pVfTgGRAxUXb6W4',
+  'authDomain': 'melclas.firebaseapp.com',
+  'databaseURL': 'https://melclas.firebaseio.com',
+  'projectId': 'melclas',
+  'storageBucket': 'melclas.appspot.com',
+  'messagingSenderId': '793958947873',
+  'appId': '1:793958947873:web:7d6142c037de883ea7d760',
+  'measurementId': 'G-RGFLCF7ZK3'
+}
+
+firebase = pyrebase.initialize_app(config)
+storage = firebase.storage()
+
+firebase_path = 'input/input'
+storage.child(firebase_path).download('backend/input/input.png')
+
+image_path = 'backend/input/input.png'
 def predict(image_path):
     cnn = load_model('backend/model')
     image = load_img(image_path, target_size=(128, 128))
     image = img_to_array(image)
     image = np.expand_dims(image, axis=0)
     prediction = int(cnn.predict(image)[0][0])
-    if prediction == 0:
-        return 'The tumor is malignant'
-    elif prediction == 1:
-        return 'The tumor is benign'
-    
+    return prediction
 
-image_path = 'backend/data/test/malignant/1.jpg'
-if __name__ == '__main__':
-    prediction = predict(image_path)
-    print(prediction)
+prediction = predict(image_path)
+if prediction == 0:
+    print('malignant')
+    storage.child('output/output').put('backend/output/malignant.png')
+elif prediction == 1:
+    print('benign')
+    storage.child('output/output').put('backend/output/benign.png')
